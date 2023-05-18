db6cea7c7676d995563c890505fcdfe438c3854a
oscarsotelo <oscaresotelo@gmail.com>
2023-05-18 14:13:10 -0300

    quinto!

--- fina2.py
+++ fina2.py
@@ -7,7 +7,7 @@ import time
 import base64
 from PIL import Image
 import pytesseract
-pytesseract.pytesseract.tesseract_cmd = r"C:\Program Files\Tesseract-OCR\tesseract.exe"
+# pytesseract.pytesseract.tesseract_cmd = r"C:\Program Files\Tesseract-OCR\tesseract.exe"
 # st.set_page_config(page_title="Gpt-AutoSustentable", page_icon=":memo:")
 hide_st_style = """
             <style>
