c26f9a067a0b340a52d48998dbea8f276e5f7246
oscar sotelo <oscaresotelo@gmail.com>
2023-05-18 18:59:14 -0300

    primercomit

--- finachat.py
+++ finachat.py
@@ -1,10 +1,41 @@
 import speech_recognition as sr
-import pyaudio
+import epitran
 
-init_rec = sr.Recognizer()
-print("Let's speak!!")
-with sr.Microphone() as source:
-    audio_data = init_rec.record(source, duration= 5)
-    print("Recognizing your text.............")
-    text = init_rec.recognize_google(audio_data)
-    print(text)
+# Initialize Epitran for English
+epi = epitran.Epitran("spa-Latn")
+
+# Initialize the speech recognizer
+r = sr.Recognizer()
+
+# Set the source of audio input
+mic = sr.Microphone()
+
+# Function to transcribe audio
+def transcribe_audio():
+    with mic as source:
+        print("Listening...")
+
+        # Adjust for ambient noise levels
+        r.adjust_for_ambient_noise(source)
+
+        # Record the audio
+        audio = r.listen(source)
+
+    print("Transcribing...")
+
+    # Use the speech recognition library to convert speech to text
+    try:
+        text = r.recognize_google(audio, language = "es-AR")
+        print("Original text:", text)
+
+        # Transliterate the text using Epitran
+        translit_text = epi.transliterate(text)
+        print("Transliterated text:", translit_text)
+
+    except sr.UnknownValueError:
+        print("Speech Recognition could not understand audio")
+    except sr.RequestError as e:
+        print("Could not request results from Speech Recognition service; {0}".format(e))
+
+# Call the function to start transcription
+transcribe_audio()
